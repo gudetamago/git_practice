838eda29ff9d4c733b983ad523ce31cd013fee90
Leanne Huang <leanne110x@gmail.com>
2025-10-07 18:51:50 +0800

    feat: update leanne branch detail

---
+++ src/data/template-user leanne.js
@@ -0,0 +1,12 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: 'Leanne Huang',
+  caption: 'She codes plus',
+  coverImageUrl:
+    'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRqC8wvz4gS3CIvkKZ-DBq0PbSeV2xn1Nhzo_6Jl5ZHbsQ-EZludTh0kQXbTjsReBzh6H0mKraNUXi4ArEW1DlzMEtFokzNc5RaKuUukno',
+  bio: "Let's code together!",
+};
+
+export default data;
