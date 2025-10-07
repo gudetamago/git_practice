fe95fa22a61799f93ab51bba83b9e29806f3332e
karoltravino <travino.karol@gmail.com>
2025-10-07 18:42:41 +0800

    feat: add Karol copy of template-user

---
+++ src/data/template-user Karol.js
@@ -0,0 +1,11 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: "Karol Travino",
+  caption: "She Codes Plus Perth student",
+  coverImageUrl: "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg",
+  bio: "I'm a She Codes Plus Perth student learnin to code!",
+};
+
+export default data;
