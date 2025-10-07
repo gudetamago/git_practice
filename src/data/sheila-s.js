ff5fc0ed65f1f586c8754cbfb9fb00ffa224b8c0
gudetamago <sharakael@yahoo.com>
2025-10-07 18:35:40 +0800

    feat: added my userfile

---
+++ src/data/sheila-s.js
@@ -0,0 +1,11 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: "Sheila S",
+  caption: "was sleepy as a child",
+  coverImageUrl: "https://jpittard.net/mortalwombat.jpg",
+  bio: ` (Husband) What do you want for dinner?`,
+};
+
+export default data;
